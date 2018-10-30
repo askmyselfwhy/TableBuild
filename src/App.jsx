3f97251f2d5be1760a2a3bb4a23c87dfc3f487cf
askmyselfwhy <romanefimov2@gmail.com>
2018-10-30 17:13:49 +0200

    Minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {
-    tableData: null
+  constructor() {
+    super();
+    this.state = {
+      tableData: null
+    }
   }
   componentDidMount() {
     fetch('http://demo4452328.mockable.io/table/1')
@@ -30,26 +33,26 @@ class App extends Component {
     return obj.children ? sumOfWides : 1;
   }
   makeTd(value, options) {
-    return <td {...options}>
+    return <td {...options} key={options.id || value}>
       {value}
     </td>
   }
-  calculateDimensionsAndPush(cells, { obj, rowsNumber, depthLevel }) {
-    let colspan = this.getWide(obj);
-    let rowspan = rowsNumber - this.getDepth(obj, depthLevel);
-    cells.push(this.makeTd(obj.value, {
+  getCell({ obj, rowsNumber, depthLevel }) {
+    let colSpan = this.getWide(obj);
+    let rowSpan = rowsNumber - this.getDepth(obj, depthLevel);
+    return this.makeTd(obj.value, {
       "valign": "top",
-      colSpan: colspan,
-      rowSpan: obj.children ? 1 : rowspan
-    }));
+      "colSpan": colSpan === 1 ? null : colSpan,
+      "rowSpan": obj.children ? null : rowSpan
+    })
   }
   buildTableHeaders(tableData) {
-    let depths = tableData.title.map(title => this.getDepth(title, 0));
+    let depths = tableData.title.map(title => this.getDepth(title, 1));
     let maxDepth = this.getMax(depths);
-    let rowsNumber = maxDepth + 1;
     let children = [...tableData.title];
     let rowsToRender = [];
-    for (let i = 0; i < rowsNumber; i++) {
+
+    for (let i = 0; i < maxDepth; i++) {
       let cells = [];
       for (let j = 0; j < tableData.title.length; j++) {
         let title = children[j];
@@ -61,11 +64,11 @@ class App extends Component {
           for (let k = 0; k < title.length; k++) {
             let currentTitle = title[k];
             if (currentTitle.value) {
-              this.calculateDimensionsAndPush(cells, {
+              cells.push(this.getCell({
                 obj: currentTitle,
-                rowsNumber: rowsNumber,
+                rowsNumber: maxDepth,
                 depthLevel: i
-              });
+              }));
               if (currentTitle.children) {
                 children[j] = children[j] ? children[j].concat(currentTitle.children) : currentTitle.children;
               }
@@ -73,37 +76,42 @@ class App extends Component {
           }
           // Если обходится 1 объект
         } else {
-          this.calculateDimensionsAndPush(cells, {
+          cells.push(this.getCell({
             obj: title,
-            rowsNumber: rowsNumber,
+            rowsNumber: maxDepth,
             depthLevel: i
-          });
+          }));
           children[j] = title.children ? [...title.children] : null;
         }
       }
-      rowsToRender.push(<tr>{cells.map(cell => cell)}</tr>)
+      rowsToRender.push(<tr key={i}>{cells}</tr>)
     }
     return rowsToRender;
   }
   buildTable(tableData) {
-    return <table border="1">
+    return <table border="1" className="app__table">
       <thead>
         {this.buildTableHeaders(tableData).map(row => row)}
       </thead>
       <tbody>
-        {tableData.content.map(row => <tr>{row.map(col => <td>{col || '-'}</td>)}</tr>)}
+        {tableData.content.map((row, index) =>
+          <tr key={index}>
+            {row.map(col => this.makeTd(col || "-", { id: col + Math.random() }))}
+          </tr>)}
       </tbody>
     </table>
   }
   render() {
     const { tableData } = this.state;
     return (
-      <div className="App">
-        {tableData ?
-          this.buildTable(tableData)
-          :
-          <div>Loading...</div>
-        }
+      <div className="app">
+        <div className="container">
+          {tableData ?
+            this.buildTable(tableData)
+            :
+            <div>Loading...</div>
+          }
+        </div>
       </div>
     );
   }
